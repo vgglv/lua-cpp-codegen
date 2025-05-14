e74fd5aedacb002840a08becaa09531d3873fc0e
vgglv <ripykt@gmail.com>
2025-05-14 23:06:52 +0900

    Testing out lua

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,25 @@
 #include "Vector2.hpp"
 #include "lua.hpp"
 
+int getVariable(lua_State* L, std::string_view variableName) {
+    lua_getglobal(L, variableName.data());
+    auto x = lua_tonumber(L, -1);
+    return static_cast<int>(x);
+}
 
 int main() {
-    game::GameObject obj;
-    obj.setXY(game::Vector2{5.f, 4.f});
-    std::cout << "Hello world!\n";
-    std::cout << "Object: [" << obj.getXY().x << ", " << obj.getXY().y << "]\n";
-    
+    lua_State* L = luaL_newstate();
+    luaL_dostring(L, "x = 42");
+    {
+        int x = getVariable(L, "x");
+        std::printf("x = %d\n", x);
+    }
+    luaL_dostring(L, "x = x + 8");
+    {
+        int x = getVariable(L, "x");
+        std::printf("x = %d\n", x);
+    }
+
+    lua_close(L);
     return 0;
 }
